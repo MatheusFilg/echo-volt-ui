74887a85031d7b1bb9c72fb2f20dfc54bd662189
matheusf <matheus_falmeida@hotmail.com>
2025-02-12 21:50:35 -0300

    add shadcn and theme toggle

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@apollo/client'
 import { getSuppliersByLimit } from './queries/get-supplier-by-limit'
 import type { Supplier } from './types/supplier'
+import { Button } from './components/ui/button'
+import { ThemeToggle } from './components/theme-toggle'
 
 export function App() {
   const inputLimit = 3001
@@ -9,16 +11,31 @@ export function App() {
     { variables: { inputLimit } }
   )
 
+  function handleSubmitTest() {
+    console.log('funcionou')
+  }
+
   return (
     <div>
+      <ThemeToggle />
       {loading ? (
         <p>Carregando...</p>
       ) : (
-        <ul>
-          {data?.supplierTable.map(supplier => (
-            <li key={supplier.id}>{supplier.name}</li>
-          ))}
-        </ul>
+        <div>
+          <ul>
+            {data?.supplierTable.map(supplier => (
+              <li key={supplier.id}>{supplier.name}</li>
+            ))}
+          </ul>
+          <Button
+            size="default"
+            variant="outline"
+            onClick={handleSubmitTest}
+            className="cursor-pointer"
+          >
+            Submit
+          </Button>
+        </div>
       )}
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { App } from './app.tsx'
 import { ApolloProvider } from '@apollo/client'
 import { client } from './lib/apollo.ts'
+import { ThemeProvider } from './components/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
     </ApolloProvider>
   </StrictMode>
 )
